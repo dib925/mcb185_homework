f37a86b3321f01d891e07b05c0ec24e84ea26383
dib925 <davidiberrios@gmail.com>
2024-01-27 19:23:14 -0800

    new

--- 21quadratic.py
+++ 21quadratic.py
@@ -12,3 +12,11 @@ def solve_quadratic (a, b, c):
 a, b, c = 2, -6, 4 
 root1, root2 = solve_quadratic(a, b, c)
 print(f"The roots are: {root1} and {root2}")
+
+a, b, c = 4, -12, 8 
+root1, root2 = solve_quadratic(a, b, c)
+print(f"The roots are: {root1} and {root2}")
+
+a, b, c = 8, -24, 12 
+root1, root2 = solve_quadratic(a, b, c)
+print(f"The roots are: {root1} and {root2}")
